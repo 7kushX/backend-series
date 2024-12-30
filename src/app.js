fa5f47f9d456e9f442691c8b65ee22d845efe663
7kushX <20kushchoudhary@gmail.com>
2024-12-30 13:31:18 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+app.use(cros({
     origin: process.env.CROS_ORIGIN,
     Credential: true
 }))
@@ -15,6 +15,15 @@ app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+//Routes
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 
 export {app}
